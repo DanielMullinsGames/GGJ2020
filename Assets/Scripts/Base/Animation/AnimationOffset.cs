8b1740e07bfa7916786e635e2aff990b6feaf448
Daniel (MSI Laptop) <daniel.mullins.games@gmail.com>
2020-02-01 14:09:50 -0800

    - art / animation / prefabs for first 2 backgrounds

---
+++ Assets/Scripts/Base/Animation/AnimationOffset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationOffset : MonoBehaviour
+{
+    private void Start()
+    {
+        var clipName = GetComponent<Animation>().clip.name;
+        GetComponent<Animation>()[clipName].normalizedTime = Random.value;
+    }
+}
