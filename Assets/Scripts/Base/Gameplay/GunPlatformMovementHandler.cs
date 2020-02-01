5cffa8480ad41002ef8e4ff4c03ffae27197d3e8
Thomas Paterson <t.o.paterson@gmail.com>
2020-02-01 00:17:47 -0800

    getting in gun platform

---
+++ Assets/Scripts/Base/Gameplay/GunPlatformMovementHandler.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GunPlatformMovementHandler : MonoBehaviour
+{
+    public float Speed;
+    public Transform CenterReference;
+    public float MaxAngle;
+    public Vector3 StartingLine;
+    public LayerMask EdgeLineMask;
+    public float StartingAngle;
+
+    private float mCurrentAngle;
+    private PlayerControlInputHandler mInputHandler;
+
+    void Start()
+    {
+        mInputHandler = GetComponent<PlayerControlInputHandler>();
+        Move(Vector2.zero);
+    }
+
+    void Update()
+    {
+        if (mInputHandler.Axis != Vector2.zero)
+        {
+            Move(mInputHandler.Axis);
+            mInputHandler.ConsumeInput();
+        }
+
+    }
+
+    private void Move(Vector2 axis)
+    {
+        mCurrentAngle = Mathf.Clamp(mCurrentAngle + axis.x * Speed * Time.deltaTime, -MaxAngle, MaxAngle);
+        Vector3 direction = Quaternion.Euler(0f, 0f, mCurrentAngle) * StartingLine;
+
+        RaycastHit2D hit = Physics2D.Raycast(CenterReference.transform.position, direction, Mathf.Infinity, EdgeLineMask);
+
+        if (hit.collider != null)
+        {
+            transform.position = hit.point;
+            transform.rotation = Quaternion.Euler(0f, 0f, mCurrentAngle + StartingAngle);
+        }
+    }
+}
---
+++ Assets/Scripts/Base/Gameplay/PlayerControlInputHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerControlInputHandler : MonoBehaviour
+{
+    public Vector2 Axis;
+    public bool Interact;
+
+    public void SendInput(Vector2 axisInput, bool interactButtonDown)
+    {
+        Axis += axisInput;
+        Interact |= interactButtonDown;
+    }
+
+    public void ConsumeInput()
+    {
+        Axis = Vector2.zero;
+        Interact = false;
+    }
+
+}
