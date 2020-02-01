fb485839cffee8f200aadeab278282224f4df9d1
Thomas Paterson <t.o.paterson@gmail.com>
2020-01-31 18:15:38 -0800

    getting some basic movement

---
+++ Assets/Scripts/Base/ManagersAndConfigs/GameStateManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+}
---
+++ Assets/Scripts/Base/ManagersAndConfigs/PlayerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConfig : MonoBehaviour
+{
+    public static PlayerConfig Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+}
