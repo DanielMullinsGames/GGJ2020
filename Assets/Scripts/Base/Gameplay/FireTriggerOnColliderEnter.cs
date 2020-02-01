9a992a71f6be68aa17390e4019e5aee129dfdf3a
Thomas Paterson <t.o.paterson@gmail.com>
2020-01-31 23:14:29 -0800

    getting a triggerable plunger into the game

---
+++ Assets/Scripts/Base/Gameplay/FireTriggerOnColliderEnter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class FireTriggerOnColliderEnter : MonoBehaviour
+{
+    public List<UnityEvent> TriggeredEvents;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (var ev in TriggeredEvents)
+            ev.Invoke();
+    }
+
+}
