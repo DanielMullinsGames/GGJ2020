f618ae8730e800a3157d38ffbd659d356337a888
Daniel (MSI Laptop) <daniel.mullins.games@gmail.com>
2020-02-01 14:49:03 -0800

    - tweening backgrounds

--- Assets/Scripts/Base/ManagersAndConfigs/EpisodeManager.cs
+++ Assets/Scripts/Base/ManagersAndConfigs/EpisodeManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
+using Pixelplacement;
 
 public class EpisodeManager : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class EpisodeManager : MonoBehaviour
 
     private List<GameObject> mChoices = new List<GameObject>();
     private GameObject mCurrentBackground;
+
+    private const float TRANSITION_DURATION = 1f;
 
     private void Awake()
     {
@@ -42,14 +45,20 @@ public class EpisodeManager : MonoBehaviour
     {
         if (next.BackgroundPrefab != null && (mCurrentBackground == null || mCurrentBackground.name != next.BackgroundPrefab.name))
         {
+            bool firstTransition = mCurrentBackground == null;
+
             if (mCurrentBackground != null)
             {
-                yield return new WaitForSeconds(1f);
-                Destroy(mCurrentBackground);
+                Tween.Position(mCurrentBackground.transform, new Vector2(-10f, 0f), TRANSITION_DURATION, 0f, Tween.EaseInOut);
             }
 
             mCurrentBackground = GameObject.Instantiate(next.BackgroundPrefab, Vector3.zero, Quaternion.identity);
             mCurrentBackground.name = next.BackgroundPrefab.name;
+
+            mCurrentBackground.transform.position = new Vector2(10f, 0f);
+            Tween.Position(mCurrentBackground.transform, Vector2.zero, firstTransition ? 0f : TRANSITION_DURATION, 0f, Tween.EaseInOut);
+
+            yield return new WaitForSeconds(TRANSITION_DURATION);
         }
     }
 }
