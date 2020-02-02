5bc46208d2b3105074362f5ca2280cad60768897
Daniel (MSI Laptop) <daniel.mullins.games@gmail.com>
2020-02-01 16:35:59 -0800

    - art / animation / implementation for score UI

---
+++ Assets/Scripts/Base/UI/ScoreBarUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBarUI : MonoBehaviour
+{
+    [SerializeField]
+    private TMPro.TextMeshPro text;
+
+    [SerializeField]
+    private TMPro.TextMeshPro shadow;
+
+    private int score;
+
+    public void UpdateScore(int score)
+    {
+        this.score = score;
+        gameObject.SetActive(true);
+        GetComponent<Animator>().Play("show_score", 0, 0f);
+    }
+
+    private void UpdateTextKeyframe()
+    {
+        text.text = shadow.text = score.ToString();
+    }
+}
---
+++ Assets/Scripts/Base/UI/ScoreUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField]
+    private List<ScoreBarUI> scoreBars;
+
+    [Header("Testing")]
+    [SerializeField]
+    private List<Motivation> testMotivations = new List<Motivation>();
+
+    public void ShowScoreForMotivation(Motivation motivation, int score)
+    {
+        var bar = scoreBars.Find(x => x.gameObject.name.ToLower().Contains(motivation.Name.ToLower()));
+
+        bar.UpdateScore(score);
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.S))
+        {
+            foreach (Motivation motivation in testMotivations)
+            {
+                ShowScoreForMotivation(motivation, 1);
+            }
+        }
+    }
+#endif
+}
