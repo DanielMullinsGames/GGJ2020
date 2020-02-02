6f47dd2686f01440afd7c36472283ffa5d10ea12
Thomas Paterson <t.o.paterson@gmail.com>
2020-02-02 10:57:49 -0800

    bit more

---
+++ Assets/Scripts/Base/Gameplay/DestroyAfterDelay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    public float Lifetime;
+
+    private void Update()
+    {
+        Lifetime -= Time.deltaTime;
+
+        if (Lifetime <= 0f)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Base/Gameplay/SpawnObjectOnDestroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjectOnDestroy : MonoBehaviour
+{
+    public GameObject ObjToSpawn;
+
+    private void OnDestroy()
+    {
+        if (ObjToSpawn != null)
+            GameObject.Instantiate(ObjToSpawn, transform.position, ObjToSpawn.transform.rotation);
+    }
+}
