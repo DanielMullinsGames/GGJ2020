48cf2ed8f280ed0b23ae02287318f9c043226b45
Thomas Paterson <t.o.paterson@gmail.com>
2020-02-02 10:57:31 -0800

    adding in some minor effects

---
+++ Assets/Scripts/Base/Gameplay/SpawnOnEnable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOnEnable : MonoBehaviour
+{
+    public GameObject ObjToSpawn;
+
+    private void OnEnable()
+    {
+        if (ObjToSpawn != null)
+            GameObject.Instantiate(ObjToSpawn, transform.position, ObjToSpawn.transform.rotation);
+    }
+}
