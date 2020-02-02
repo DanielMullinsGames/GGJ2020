aaaae86d0ca3663c3d6b0bbf133328c7ed4f5b67
Daniel (MSI Laptop) <daniel.mullins.games@gmail.com>
2020-02-02 14:41:51 -0800

    - end screen

--- Assets/Scripts/Base/UI/ScorePlatform.cs
+++ Assets/Scripts/Base/UI/ScorePlatform.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ScorePlatform : MonoBehaviour
 {
+    public int platformIndex;
+
     [SerializeField]
     private TMPro.TextMeshPro winnerText;
 
@@ -24,7 +26,33 @@ public class ScorePlatform : MonoBehaviour
 
     void Start()
     {
-        
+        var scores = new List<MotivationScore>(MotivationManager.SavedScores);
+
+        if (scores.Count < platformIndex + 1)
+        {
+            transform.position = Vector3.one * 10000f;
+            return;
+        }
+
+        scores.Sort((MotivationScore a, MotivationScore b) => 
+        {
+            if (a.Score > b.Score)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        });
+
+        ShowScoreText((int)scores[platformIndex].Score);
+        ShowMotivation(scores[platformIndex].Type);
+
+        if (platformIndex == 0)
+        {
+            ShowWinnerText(scores[platformIndex].Type);
+        }
     }
 
     private void ShowMotivation(Motivation motivation)
@@ -39,6 +67,6 @@ public class ScorePlatform : MonoBehaviour
 
     private void ShowWinnerText(Motivation motivation)
     {
-        winnerText.text = winnerShadow.text = "In the end, it was " + motivation.Name + " that truly drove him.";
+        winnerText.text = winnerShadow.text = ("In the end, it was " + motivation.Name + " that truly drove him.").ToUpper();
     }
 }
