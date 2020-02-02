f9b5b094c9e52fa74d6feeebcecf57abf455085a
Daniel (MSI Laptop) <daniel.mullins.games@gmail.com>
2020-02-01 19:36:33 -0800

    - desaturate backgrounds during play

---
+++ Assets/Scripts/Base/Rendering/DesaturateDuringPlay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class DesaturateDuringPlay : MonoBehaviour
+{
+    [SerializeField]
+    private float fadeSpeed = 1f;
+
+    private void Update()
+    {
+        bool desaturate = GameStateManager.Instance.Playing;
+
+        var curves = GetComponent<ColorCorrectionCurves>();
+        curves.saturation = Mathf.Lerp(curves.saturation, desaturate ? 0f : 1f, Time.deltaTime * fadeSpeed);
+    }
+}
