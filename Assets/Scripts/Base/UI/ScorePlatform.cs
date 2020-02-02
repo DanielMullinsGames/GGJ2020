d3d79ea5a6d39f6a3d426761eb7240b51c2eb865
Daniel (MSI Laptop) <daniel.mullins.games@gmail.com>
2020-02-02 14:32:03 -0800

    - end screen

---
+++ Assets/Scripts/Base/UI/ScorePlatform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePlatform : MonoBehaviour
+{
+    [SerializeField]
+    private TMPro.TextMeshPro winnerText;
+
+    [SerializeField]
+    private TMPro.TextMeshPro winnerShadow;
+
+    [SerializeField]
+    private TMPro.TextMeshPro scoreText;
+
+    [SerializeField]
+    private TMPro.TextMeshPro scoreShadow;
+
+    [SerializeField]
+    private List<GameObject> motiveAnims = new List<GameObject>();
+
+    [SerializeField]
+    private bool winnerPlatform;
+
+    void Start()
+    {
+        
+    }
+
+    private void ShowMotivation(Motivation motivation)
+    {
+        motiveAnims.Find(x => x.name.ToLower().Contains(motivation.Name.ToLower())).SetActive(true);
+    }
+
+    private void ShowScoreText(int score)
+    {
+        scoreText.text = scoreShadow.text = score.ToString();
+    }
+
+    private void ShowWinnerText(Motivation motivation)
+    {
+        winnerText.text = winnerShadow.text = "In the end, it was " + motivation.Name + " that truly drove him.";
+    }
+}
