31f440a4148f27bdc4e082374415d56c68e535e3
Daniel (MSI Laptop) <daniel.mullins.games@gmail.com>
2020-02-01 18:21:19 -0800

    - one way platforms

---
+++ Assets/Scripts/Base/Gameplay/IgnoreCollisionVolume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollisionVolume : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D colliderToIgnore;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<CharacterController2D>())
+        {
+            Physics2D.IgnoreCollision(colliderToIgnore, other, ignore: true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<CharacterController2D>())
+        {
+            Physics2D.IgnoreCollision(colliderToIgnore, other, ignore: false);
+        }
+    }
+}
